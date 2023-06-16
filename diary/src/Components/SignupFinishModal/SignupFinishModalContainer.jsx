bbe7f0dedad701010b1a8b247bd251f56f621dd7
성은지 <96868722+eunji1@users.noreply.github.com>
2023-06-16 09:37:51 +0900

    router.query를 이용해 email인증 정보 나타냄 IsEmailPage대신 router.query.email

--- diary/src/Components/SignupFinishModal/SignupFinishModalContainer.jsx
+++ diary/src/Components/SignupFinishModal/SignupFinishModalContainer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import SignupFinishModalPresenter from './SignupFinishModalPresenter';
 
-const SignupFinishModalContainer = ({ setIsSignup }) => (
-  <SignupFinishModalPresenter setIsSignup={setIsSignup} />
-);
+const SignupFinishModalContainer = ({ setIsSignup }) => {
+  const router = useRouter();
+  const { email } = router.query || '';
+  return (
+    <SignupFinishModalPresenter setIsSignup={setIsSignup} email={email} />
+  );
+};
 SignupFinishModalContainer.propTypes = {
   setIsSignup: PropTypes.func.isRequired,
 };
--- diary/src/Components/SignupFinishModal/SignupFinishModalPresenter.jsx
+++ diary/src/Components/SignupFinishModal/SignupFinishModalPresenter.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { LOGIN, SIGNUP } from 'src/Constants/constants';
+import { LOGIN_CONST } from 'src/Constants/LoginConstant';
+import { SIGNUP_CONST } from 'src/Constants/signupConstant';
+import Button from '../Button/Button';
 
-const SignupFinishModalPresenter = ({ setIsSignup }) => (
+const SignupFinishModalPresenter = ({ setIsSignup, email }) => (
   <div className="flex flex-col justify-center items-center">
-    <p className="w-72 text-center text-gray-400 bg-gray-200 p-3 mt-10">ssss1111sss@diary.com</p>
-    <button
-      type="button"
+    <p className="w-72 text-center text-gray-600 bg-gray-200 p-3 mt-10">{email}</p>
+    <Button
       onClick={() => setIsSignup(false)}
       className="w-72 bg-orange-300 text-white rounded-md mt-10 p-2.5"
-    >
-      {LOGIN.CONTENT_KR}
-    </button>
+      content={LOGIN_CONST.CONTENT_KR}
+    />
     <div className="text-sm text-center mt-2">
-      <span className="text-gray-700">{SIGNUP.EMAIL_ISNOT_SEND}</span>
+      <span className="text-gray-700">{SIGNUP_CONST.EMAIL_ISNOT_SEND}</span>
       <span
         onClick={() => console.log('이메일을 다시 보냈습니다.')}
         aria-hidden="true"
         type="button"
         className="w-fit text-red-600 cursor-pointer hover:underline"
       >
-        {SIGNUP.EMAIL_RESEND}
+        {SIGNUP_CONST.EMAIL_RESEND}
       </span>
     </div>
   </div>
 );
 SignupFinishModalPresenter.propTypes = {
   setIsSignup: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
 };
 export default SignupFinishModalPresenter;
